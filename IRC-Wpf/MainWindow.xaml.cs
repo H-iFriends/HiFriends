f46d378f7ff8b674b56a174a22c2e199f3f22770
Drrrring <y6655533@163.com>
2022-05-27 09:08:57 +0800

    add: server operations

--- IRC-Wpf/MainWindow.xaml.cs
+++ IRC-Wpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace IRC_Wpf
 {
@@ -20,6 +22,7 @@ namespace IRC_Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> Servers = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
@@ -31,6 +34,49 @@ namespace IRC_Wpf
 
         private void ConnectButton_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void AddServer(string s)
+        {
+            if (Servers.Contains(s))
+                return;
+            Servers.Add(s);
+        }
+
+        private void RemoveServer(string s)
+        {
+            if(Servers.Contains(s))
+                Servers.Remove(s);
+        }
+
+        private List<string> FindServer(string s)
+        {
+            List<string> res = new List<string>();
+            foreach(string server in Servers)
+            {
+                if(server.Contains(s))
+                    res.Add(server);
+            }
+            return res;
+        }
+
+        private void ExportServer(string filePath = "servers.xml")
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<string>));
+            using(FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                xmlSerializer.Serialize(fs, Servers);
+            }
+        }
+
+        private List<string> ImportServer(string filePath = "servers.xml")
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<string>));
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                return (List<string>)xmlSerializer.Deserialize(fs);
+            }
 
         }
     }
