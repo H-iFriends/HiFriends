49835e9bfd6132d3d841d78bd18cb2b3f9c5305b
Drrrring <y6655533@163.com>
2022-05-27 10:37:34 +0800

    fix: new project for business logic

---
+++ IRC-Business/ContentFiller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRC_Business
+{
+    /*
+     * 实现智能补全
+     */
+    internal class ContentFiller
+    {
+    }
+}
--- IRC-Wpf/MainWindow.xaml.cs
+++ IRC-Wpf/MainWindow.xaml.cs
@@ -22,13 +22,17 @@ namespace IRC_Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> Servers = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
 
             //从文件中读取保存的server
-            ImportServer();
+            //ImportServer();
+
+            //绑定数据到显示列表中
+            //this.server_list.ItemsSource = Servers;
+
+            
         }
         private void SelectButton_Click(object sender, EventArgs e)
         {
@@ -40,56 +44,6 @@ namespace IRC_Wpf
 
         }
 
-        private void AddServer(string s)
-        {
-            if (Servers.Contains(s))
-                return;
-            Servers.Add(s);
-        }
-
-        private void RemoveServer(string s)
-        {
-            if (Servers.Contains(s))
-                Servers.Remove(s);
-        }
-
-        private List<string> FindServer(string s)
-        {
-            List<string> res = new List<string>();
-            foreach (string server in Servers)
-            {
-                if (server.Contains(s))
-                    res.Add(server);
-            }
-            return res;
-        }
-
-        private void ExportServer(string filePath = "servers.xml")
-        {
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<string>));
-            using (FileStream fs = new FileStream(filePath, FileMode.Create))
-            {
-                xmlSerializer.Serialize(fs, Servers);
-            }
-        }
-
-        private List<string> ImportServer(string filePath = "servers.xml")
-        {
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<string>));
-            try
-            {
-                using (FileStream fs = new FileStream(filePath, FileMode.Open))
-                {
-                    return (List<string>)xmlSerializer.Deserialize(fs);
-                }
-            }catch(FileNotFoundException e)
-            {
-                Console.WriteLine("File \"servers.xml\" not found.");
-                return new List<string>();
-            }
-
-
-
-        }
+        
     }
 }
