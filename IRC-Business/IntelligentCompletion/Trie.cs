2de60fff15d0b7be6715d1c8a9a954b449628fae
Drrrring <y6655533@163.com>
2022-06-19 22:45:55 +0800

    .gitignore

---
+++ IRC-Business/IntelligentCompletion/Trie.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRC_Business.IntelligentCompletion
+{
+    internal class Trie
+    {
+        private TrieNode trie;
+        
+        public Trie()
+        {
+            trie = new TrieNode();
+        }
+
+        public void AddVocabulary(string s)
+        {
+            if(s == null || s.Length == 0)
+            {
+                throw new ArgumentException("Input vocabulary is null or empty.");
+            }
+            
+            TrieNode current = trie;
+            
+            
+            //将词语加入字典树
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(i != s.Length - 1)
+                {
+                    current.isEnd = false;
+                }
+                //找到下一个节点
+                bool flag = true;
+                foreach(TrieNode node in current.next)
+                {
+                    if(node.character == s[i])
+                    {
+                        current = node;
+                        flag = false;
+                        break;
+                    }
+                }
+                //若在后继节点数组中未找到想找的节点，则新增进去
+                if (flag)
+                {
+                    current = new TrieNode(s[i]);
+                    current.next.Add(current);
+                }
+            }
+        }
+    }
+}
---
+++ IRC-Business/IntelligentCompletion/TrieNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRC_Business.IntelligentCompletion
+{
+    public class TrieNode
+    {
+        public List<TrieNode> next;
+        public bool isEnd;
+        public char character;
+
+        public TrieNode()
+        {
+            next = new List<TrieNode>();
+            isEnd = true;
+            character = '0';
+        }
+
+        public TrieNode(char character)
+        {
+            next = new List<TrieNode>();
+            isEnd = true;
+            this.character = character;
+        }
+
+        public TrieNode(List<TrieNode> next, bool isEnd, char character)
+        {
+            this.next = next;
+            this.isEnd = isEnd;
+            this.character = character;
+        }
+    }
+}
