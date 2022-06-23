36ea5a5a5b408701bf41f6ae2b1cec7e9e6e991a
Ameixa551 <ameixa@163.com>
2022-06-23 14:08:07 +0800

    聊天窗口数据绑定

--- IRC-Wpf/App.xaml.cs
+++ IRC-Wpf/App.xaml.cs
@@ -13,5 +13,14 @@ namespace IRC_Wpf
     /// </summary>
     public partial class App : Application
     {
+        //热点话题选中后将其添加到当前聊天室列表
+        private void HotListViewItem_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show("已添加该聊天室","HiFriends",MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+        private void JoinedListViewItem_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show("已进入该聊天室", "HiFriends", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
--- IRC-Wpf/ChatWindow.xaml.cs
+++ IRC-Wpf/ChatWindow.xaml.cs
@@ -22,7 +22,9 @@ namespace IRC_Wpf
         public ChatWindow()
         {
             InitializeComponent();
+            initWindow();
         }
+        //按钮点击事件
         private void SendButton_Click(object sender, RoutedEventArgs e)
         {
 
@@ -40,6 +42,46 @@ namespace IRC_Wpf
         private void delChatRoom_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        //列表表项点击事件
+        private void HotListDataBinding_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            ChatRoom? chatRoomSelected = HotListDataBinding.SelectedItem as ChatRoom;
+            if(chatRoomSelected !=null && chatRoomSelected is ChatRoom)
+            {
+                MessageBox.Show("选中的聊天室:" + chatRoomSelected.Name);
+            }
+        }
+
+        //初始化窗口（传入数据）
+        private void initWindow()
+        {
+            //热点聊天室列表数据绑定
+            List<ChatRoom> hotChatRooms = new List<ChatRoom>();
+            hotChatRooms.Add(new ChatRoom() { Activity = 20 , Topic = "足球",Name="体育聊天室" });
+            hotChatRooms.Add(new ChatRoom() { Activity = 33, Topic = "软件构造",Name="课程聊天室" });
+            HotListDataBinding.ItemsSource = hotChatRooms;
+
+            //已加入聊天室列表数据绑定
+            List<ChatRoom> joinedChatRooms = new List<ChatRoom>();
+            joinedChatRooms.Add(new ChatRoom() { Name = "王者荣耀" });
+            joinedChatRooms.Add(new ChatRoom() { Name = "英雄联盟" });
+            JoinedListDataBinding.ItemsSource = joinedChatRooms;
+        }
+    }
+
+    //用来测试数据绑定，后面删掉传入正常数据list就行，写在里面的都是必须存在的东西
+    public class ChatRoom
+    {
+        public string Topic { get; set; }   
+        public int Activity { get; set; }
+        public string Name { get; set; }
+
+        //展示已加入聊天室列表时需要用到tostring方法
+        public override string ToString()
+        {
+            return this.Name;
         }
     }
 }
