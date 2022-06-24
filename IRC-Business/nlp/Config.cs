e93e11ecaad6e8af93380d18b525c750bd89bcfd
Drrrring <y6655533@163.com>
2022-06-24 23:00:42 +0800

    Revert "add: filter unused & duplicate"
    This reverts commit e76081eb9f9e8453fe6cdd674b66dacf537574c6.

--- IRC-Business/nlp/Config.cs
+++ IRC-Business/nlp/Config.cs
@@ -6,7 +6,7 @@ public class Config {
 	 * Please do not commit the secrets to the repository.
 	 */
 	
-	public const string SECRET_ID = "AKIDsa2FDwPdcMq79vRkocATWNzPcaIlOuBD";
+	public const string SECRET_ID = "";
 	
-	public const string SECRET_KEY = "pPjodDXcT5SZR4TdbP0MTKwIOFwNYXIq";
+	public const string SECRET_KEY = "";
 }
--- IRC-Business/nlp/NLP.cs
+++ IRC-Business/nlp/NLP.cs
@@ -5,54 +5,32 @@ using TencentCloud.Nlp.V20190408;
 using TencentCloud.Nlp.V20190408.Models;
 
 public class NLP {
-	
-	private static readonly HashSet<string> acceptablePos = new(new string[] {
-		"a",
-		"ad",
-		"al",
-		"an",
-		"b",
-		// "d",
-		// "f",
-		"g",
-		"gb",
-		"gbc",
-		"gc",
-		"gg",
-		"gi",
-		"gm",
-		"gp",
-		"i",
-		"l",
-		"v",
-		"vd",
-		"vf",
-		"vg",
-		"vi",
-		"vl",
-		"vn",
-		"vx",
-	});
-
-	public static LexicalAnalysisResponse LexicalAnalysis(string text) {
+	public static void LexicalAnalysis(string text) {
 		var c = new NlpClient(new Credential {
 			SecretId = Config.SECRET_ID,
 			SecretKey = Config.SECRET_KEY
 		}, "ap-guangzhou");
 
-		return c.LexicalAnalysis(new LexicalAnalysisRequest {
+		var r = c.LexicalAnalysis(new LexicalAnalysisRequest {
 			Text = text
 		}).Result;
-	}
-
-	public static string[] GetKeywords(string text) {
-		var response = LexicalAnalysis(text);
-		var keywordSet = new HashSet<string>();
-		foreach (var t in response.PosTokens) {
-			if (!t.Pos.StartsWith('n') && !acceptablePos.Contains(t.Pos))
-				continue;
-			keywordSet.Add($"{t.Word}({t.Pos})");
+		
+		if (r == null) {
+			Console.WriteLine("null");
+			return;
 		}
-		return keywordSet.ToArray();
+		
+		Console.WriteLine("PosTokens:");
+		foreach (var token in r.PosTokens) {
+			Console.WriteLine($"Offset: {token.BeginOffset}, Length: {token.Length}, Word: {token.Word}, Pos: {token.Pos}");
+		}
+		
+		Console.WriteLine("\n\n");
+		
+		Console.WriteLine("NerTokens:");
+		foreach (var token in r.NerTokens) {
+			Console.WriteLine($"Offset: {token.BeginOffset}, Length: {token.Length}, Word: {token.Word}, Type: {token.Type}");
+		}
+		
 	}
 }
