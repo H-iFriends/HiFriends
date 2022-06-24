e76081eb9f9e8453fe6cdd674b66dacf537574c6
Eat-Swap <root@eatswap.org>
2022-06-24 22:33:18 +0800

    add: filter unused & duplicate

--- IRC-Business/nlp/NLP.cs
+++ IRC-Business/nlp/NLP.cs
@@ -5,32 +5,54 @@ using TencentCloud.Nlp.V20190408;
 using TencentCloud.Nlp.V20190408.Models;
 
 public class NLP {
-	public static void LexicalAnalysis(string text) {
+	
+	private static readonly HashSet<string> acceptablePos = new(new string[] {
+		"a",
+		"ad",
+		"al",
+		"an",
+		"b",
+		// "d",
+		// "f",
+		"g",
+		"gb",
+		"gbc",
+		"gc",
+		"gg",
+		"gi",
+		"gm",
+		"gp",
+		"i",
+		"l",
+		"v",
+		"vd",
+		"vf",
+		"vg",
+		"vi",
+		"vl",
+		"vn",
+		"vx",
+	});
+
+	public static LexicalAnalysisResponse LexicalAnalysis(string text) {
 		var c = new NlpClient(new Credential {
 			SecretId = Config.SECRET_ID,
 			SecretKey = Config.SECRET_KEY
 		}, "ap-guangzhou");
 
-		var r = c.LexicalAnalysis(new LexicalAnalysisRequest {
+		return c.LexicalAnalysis(new LexicalAnalysisRequest {
 			Text = text
 		}).Result;
-		
-		if (r == null) {
-			Console.WriteLine("null");
-			return;
+	}
+
+	public static string[] GetKeywords(string text) {
+		var response = LexicalAnalysis(text);
+		var keywordSet = new HashSet<string>();
+		foreach (var t in response.PosTokens) {
+			if (!t.Pos.StartsWith('n') && !acceptablePos.Contains(t.Pos))
+				continue;
+			keywordSet.Add($"{t.Word}({t.Pos})");
 		}
-		
-		Console.WriteLine("PosTokens:");
-		foreach (var token in r.PosTokens) {
-			Console.WriteLine($"Offset: {token.BeginOffset}, Length: {token.Length}, Word: {token.Word}, Pos: {token.Pos}");
-		}
-		
-		Console.WriteLine("\n\n");
-		
-		Console.WriteLine("NerTokens:");
-		foreach (var token in r.NerTokens) {
-			Console.WriteLine($"Offset: {token.BeginOffset}, Length: {token.Length}, Word: {token.Word}, Type: {token.Type}");
-		}
-		
+		return keywordSet.ToArray();
 	}
 }
